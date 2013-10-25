c6a1d11b5b560f33d255b6bc649be5ef941bc30c
armani <armani@gmail.com>
2013-10-24 23:43:21 -0200

    Limpeza novo Branch parte 2

--- Backup/T-SHAPED/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('location: ./controller/teste.php');
-?>
--- T-SHAPED0710/controller/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    unset($_SESSION);
-    session_destroy(); 
-
-    header('location: ../controller/exec.php');
-    exit;
-
-?>
